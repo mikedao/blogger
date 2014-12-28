5471dcc3edcb4e9888eb6c75631fdef53fecaf49
Michael Dao <michael.dao@gmail.com>
2014-12-28 16:03:39 -0700

    I2 Complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,17 +7,20 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
     @article = Article.new
+
   end
 
   def create
     @article = Article.new(article_params)
     @article.save
     flash.notice = "Article '#{@article.title}' CREATED!"
-    
+
     redirect_to article_path(@article)
   end
 
